58b21772776ccb04e3733bac14a5bd4b7b6cb0ad
unknown <prateekchourasiya25@gmail.com>
2023-10-19 13:06:38 +0530

    first commit

--- models/connection.js
+++ models/connection.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false);
+const mongoose = require("mongoose"); 
+mongoose.set("strictQuery", false);
 
 // mongoose.connect("mongodb+srv://prateek_31:prteek2123@web-integratorz-ems.b4qq5bi.mongodb.net/")
 //   .then(function () {
@@ -7,19 +7,22 @@ mongoose.set('strictQuery', false);
 
 //   })
 
-
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb").MongoClient;
 
 const username = encodeURIComponent("prateek_31");
 const password = encodeURIComponent("prteek2123");
-// Replace the uri string with your connection string.
-const uri ="mongodb+srv://${username}:${password}@web-integratorz-ems.b4qq5bi.mongodb.net/main"; 
-mongoose.connect(uri,{
-  useNewUrlParser: true,
-   useUnifiedTopology: true,
-}).then(()=>{
-  console.log("connected");
-}).catch((err)=>{
-  console.error(err.stack)
-  process.exit(1)
-})
+const uri =
+  `mongodb://${username}:${password}@ac-gv778bu-shard-00-00.b4qq5bi.mongodb.net:27017,ac-gv778bu-shard-00-01.b4qq5bi.mongodb.net:27017,ac-gv778bu-shard-00-02.b4qq5bi.mongodb.net:27017/?ssl=true&replicaSet=atlas-nknmue-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  });
