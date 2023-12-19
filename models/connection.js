c6f73e89f5cee2c6a62be9330697636dc063d85c
prateek031 <prateekchourasiya25@gmail.com>
2023-12-19 17:47:19 +0530

    this push to deploy

--- models/connection.js
+++ models/connection.js
@@ -3,7 +3,9 @@ mongoose.set("strictQuery", false);
 const { MongoClient } = require("mongodb").MongoClient;
 
 
-const uri = process.env.URL;
+// const uri = process.env.URL;
+
+const uri = "mongodb://prateek_31:prteek2123@ac-gv778bu-shard-00-00.b4qq5bi.mongodb.net:27017,ac-gv778bu-shard-00-01.b4qq5bi.mongodb.net:27017,ac-gv778bu-shard-00-02.b4qq5bi.mongodb.net:27017/?ssl=true&replicaSet=atlas-nknmue-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose
   .connect(uri, {
--- models/userModel.js
+++ models/userModel.js
@@ -8,32 +8,32 @@ const userModel = new mongoose.Schema({
   },
   username: {
     type: String,
-    trim: true,
-    unique: true,
-    required: [true, "Username field must not be empty"],
-    minLength: [4, "Username field must have atleast 4 characters"]
+    // trim: true,
+    // unique: true,
+    // required: [true, "Username field must not be empty"],
+    // minLength: [4, "Username field must have atleast 4 characters"]
   },
   name: {
     type: String,
-    trim: true,
-    required: [true, "Name field must not be empty"],
-    minLength: [2, "Name field must have atleast 2 characters"]
+    // trim: true,
+    // required: [true, "Name field must not be empty"],
+    // minLength: [2, "Name field must have atleast 2 characters"]
   },
   password: String,
   email: {
     type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: [true, "Email address is required"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Invalid email address",
-    ],
+    // trim: true,
+    // lowercase: true,
+    // unique: true,
+    // required: [true, "Email address is required"],
+    // match: [
+    //   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    //   "Invalid email address",
+    // ],
   },
   avatar: {
     type: String,
-    default: "default.avif",
+    // default: "default.avif",
   },
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "tasks" }],
   updates: [
